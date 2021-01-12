c3794048665612098925c96e5f0ce72a945d0dfc
Abhi kanchan <37064943+abhishekc94@users.noreply.github.com>
2021-01-12 13:28:07 +0530

    First Commit

---
+++ STSGithubProject/src/main/java/com/abhi/STSGithubProject/StsGithubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.abhi.STSGithubProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsGithubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsGithubProjectApplication.class, args);
+	}
+
+}
---
+++ STSGithubProject/src/test/java/com/abhi/STSGithubProject/StsGithubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.STSGithubProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsGithubProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
